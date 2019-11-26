8831f107a0c2d0ee33594338b2fd6148e175e6fd
Heikki <heikkimat@gmail.com>
2019-11-26 14:05:52 +0200

    first version

---
+++ app.js
@@ -0,0 +1,52 @@
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+const grid = () => Array.from(document.getElementsByClassName('q'));
+//helper functions
+const qNumId = (qEl) => Number.parseInt(qEl.id.replace('q', ''));
+const emptyQs = () => grid().filter(_qEl => _qEl.innerText ==='');
+const isAllSame = (arr) => arr.every(_qEl => _qEl.innerText === arr[0].innerText && _qEl.innerText !== '');
+const takeTurn = (index, letter) => grid()[index].innerText = letter;
+const opponentChoice = () => qNumId(emptyQs()[Math.floor(Math.random()*emptyQs().length)]);
+const endGame = (winningSequence) => {
+    winningSequence.forEach(_qEl => _qEl.classList.add('winner'));
+    disableListeners();
+}
+const checkForVictory = () => {
+    let victory = false;
+    winningCombos.forEach(_c =>{
+        const _grid = grid();
+        const sequence = [_grid[_c[0]], _grid[_c[1]], _grid[_c[2]]];
+        if(isAllSame(sequence)) {
+            victory = true;
+            endGame(sequence);
+        }
+    });
+    return victory;
+}
+const opponentTurn = () => {
+    disableListeners();
+    setTimeout(() => {
+        takeTurn(opponentChoice(), 'o');
+        if(!checkForVictory())
+            enableListeners();
+    }, 1000);
+};
+
+const clickFn = ($event) => {
+    takeTurn(qNumId($event.target), 'x');
+    if(!checkForVictory())
+        opponentTurn();
+};
+
+//listeners
+const enableListeners = () => grid().forEach(_qEl => _qEl.addEventListener('click', clickFn));
+const disableListeners = () => grid().forEach(_qEl => _qEl.removeEventListener('click', clickFn));
+enableListeners();
